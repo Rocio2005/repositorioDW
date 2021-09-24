244802a5fdfd41af9bfa91ce5da873264a48e50e
Rocio2005 <rochi1605@gmail.com>
2021-09-23 21:39:48 -0300

    Ejercicio 1,modulo3

---
+++ Modulo 3/ejercicio1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+    <?php $primernumero=8;
+    $segundonumero=5;
+    $resto=$primernumero%5;
+    $cociente=$primernumero/$segundonumero;
+    $suma=$primernumero+$segundonumero;
+
+
+    echo "El resto es igual a: ".$resto."  ";
+    echo "El cociente es igual a: ".$cociente."  ";
+    echo "La suma es igual a: ".$suma."  " ;
+
+    ?>
+</body>
+</html>
